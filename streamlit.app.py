9fa00649151b594d310c7425e4bfcd2a7a6a664c
k-mamatha <131872394+k-mamatha@users.noreply.github.com>
2023-04-28 14:02:23 +0530

    Update streamlit.app.py

--- streamlit.app.py
+++ streamlit.app.py
@@ -27,7 +27,7 @@ streamlit.dataframe(fruits_to_show)
 
 #New section to display fruityvice api response
 streamlit.header("Fruityvice Fruit Advice!")
-fruit_choice = streamlit.text_input('What fruit would you like information about?', 'Orange')
+fruit_choice = streamlit.text_input('What fruit would you like information about?', 'Kiwi')
 streamlit.write('The user entered', fruit_choice)
 
 #import requests
@@ -55,4 +55,4 @@ add_my_fruit = streamlit.text_input('What fruit would you like to add?', 'Cantal
 streamlit.write('Thanks for adding', add_my_fruit)
 
 #This will not work correctly, but just go with it for now
-my_cur.execute("insert into PC_RIVERY_DB.PUBLIC.FRUIT_LOAD_LIST values ('from streamlit')")
+#my_cur.execute("insert into PC_RIVERY_DB.PUBLIC.FRUIT_LOAD_LIST values ('from streamlit')")
