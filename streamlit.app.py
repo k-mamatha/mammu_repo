4a590bd6b697ece3c934ba94670d83174bf90f23
k-mamatha <131872394+k-mamatha@users.noreply.github.com>
2023-04-28 15:27:03 +0530

    Update streamlit.app.py

--- streamlit.app.py
+++ streamlit.app.py
@@ -13,7 +13,6 @@ streamlit.text('🐔 Hard-Boiled Free-Range Egg')
 streamlit.text(' 🥑🍞 Avacado Toast')
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
-
 #import pandas
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
@@ -29,10 +28,10 @@ streamlit.dataframe(fruits_to_show)
 def get_fruityvice_data(this_fruit_choice):
        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + this_fruit_choice)
         fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-        returned fruityvice_normalized
+        return fruityvice_normalized
     
 #New section to display fruityvice api response
-streamlit.header("Fruityvice Fruit Advice!")
+streamlit.header('Fruityvice Fruit Advice!')
 try:
     fruit_choice = streamlit.text_input('What fruit would you like information about?')
     if not fruit_choice:
@@ -44,7 +43,6 @@ try:
  except URLError as e:
   streamlit.error()
 
-  
 streamlit.write('The user entered', fruit_choice)
 
 #import requests
